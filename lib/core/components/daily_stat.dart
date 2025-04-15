5995098ac3dadae39ed3d136bad32feac3a30153
mohamed hisham <mohamedhis36@gmail.com>
2025-04-15 15:19:08 +0200

    refactored the daily stats into there own file

---
+++ lib/core/components/daily_stat.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class DailyStat extends StatelessWidget {
+  const DailyStat({
+    super.key,
+    required this.day,
+    required this.height,
+    this.earnings = 0.00,
+  });
+
+  final String day;
+  final double height;
+  final double earnings;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(earnings == 0 ? '' : '$earnings₹', style: TextStyle(fontSize: 12)),
+        Container(
+          width: 8,
+          height: height,
+          decoration: BoxDecoration(
+            color: Theme.of(context).secondaryHeaderColor,
+            borderRadius: BorderRadius.circular(4),
+          ),
+        ),
+        Text(day),
+      ],
+    );
+  }
+}
