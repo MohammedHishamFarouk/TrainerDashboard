17cdfd046732e7a2e0d251f2916ff84afb0ec16f
mohamed hisham <mohamedhis36@gmail.com>
2025-04-15 19:33:39 +0200

    finished the navigation_bar_screen.dart logic with the provider and added dummy screens

--- lib/view/navigation_bar_screen.dart
+++ lib/view/navigation_bar_screen.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:trainer_dashboard/constants/assets.dart';
-import 'package:trainer_dashboard/home_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:trainer_dashboard/view/calendar_screen.dart';
+import 'package:trainer_dashboard/view/core/constants/assets.dart';
+import 'package:trainer_dashboard/view/home_screen.dart';
+import 'package:trainer_dashboard/view/jobs_screen.dart';
+import 'package:trainer_dashboard/view/settings_screen.dart';
+import 'package:trainer_dashboard/viewModel/navigation_bar_provider.dart';
 
 class NavigationBarScreen extends StatelessWidget {
   const NavigationBarScreen({super.key});
 
+  final List<Widget> screens = const [
+    HomeScreen(),
+    CalendarScreen(),
+    JobsScreen(),
+    SettingsScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
+    final navProvider = Provider.of<NavigationBarProvider>(context);
+
     return Scaffold(
-      body: HomeScreen(),
+      body: screens[navProvider.selectedIndex],
       floatingActionButton: SizedBox(
         width: 50,
         height: 50,
         child: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () => Navigator.pushNamed(context, '/dummyScreen'),
           child: Image(image: AssetImage(Assets.plusIcon)),
         ),
       ),
@@ -24,19 +38,19 @@ class NavigationBarScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             IconButton(
-              onPressed: () {},
+              onPressed: () => navProvider.setIndex(0),
               icon: Image(image: AssetImage(Assets.homeIcon)),
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () => navProvider.setIndex(1),
               icon: Image(image: AssetImage(Assets.calendarIcon)),
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () => navProvider.setIndex(2),
               icon: Image(image: AssetImage(Assets.jobsIcon)),
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () => navProvider.setIndex(3),
               icon: Image(image: AssetImage(Assets.settingsIcon)),
             ),
           ],
