68255e225ca195a3de7e4975e2f49ba28100dd95
mohamed hisham <mohamedhis36@gmail.com>
2025-04-15 22:05:28 +0200

    added the calendar view

--- lib/view/calendar_screen.dart
+++ lib/view/calendar_screen.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:trainer_dashboard/view/core/style/color_manager.dart';
 
-class CalendarScreen extends StatelessWidget {
+class CalendarScreen extends StatefulWidget {
   const CalendarScreen({super.key});
 
   @override
+  State<CalendarScreen> createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Calendar Screen')));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calendar'),
+        automaticallyImplyLeading: false,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: TableCalendar(
+          firstDay: DateTime.utc(2020, 1, 1),
+          lastDay: DateTime.utc(2030, 12, 31),
+          focusedDay: _focusedDay,
+          selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+          onDaySelected: (selectedDay, focusedDay) {
+            setState(() {
+              _selectedDay = selectedDay;
+              _focusedDay = focusedDay;
+            });
+          },
+          calendarStyle: const CalendarStyle(
+            selectedDecoration: BoxDecoration(
+              color: Colors.blue,
+              shape: BoxShape.circle,
+            ),
+            todayDecoration: BoxDecoration(
+              color: ColorManager.darkCyan,
+              shape: BoxShape.circle,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
