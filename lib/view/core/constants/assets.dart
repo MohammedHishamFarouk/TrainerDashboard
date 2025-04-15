b523e7d1b1ea68bf50d86733d07171f4bccfb998
mohamed hisham <mohamedhis36@gmail.com>
2025-04-15 20:23:37 +0200

    adjusted the grey color and added the dark and light cyan colors

--- lib/view/core/constants/assets.dart
+++ lib/view/core/constants/assets.dart
@@ -1,5 +1,7 @@
 abstract class Assets {
   static const String noProfilePic = 'assets/no_profile_pic.jpg';
+  static const String profile1 = 'assets/trial1.jpg';
+  static const String profile2 = 'assets/trial2.jpg';
   static const String verifiedIcon = 'assets/icons/verified_icon.png';
   static const String bellIcon = 'assets/icons/bell_icon.png';
   static const String plusIcon = 'assets/icons/plus_icon.png';
@@ -7,4 +9,6 @@ abstract class Assets {
   static const String calendarIcon = 'assets/icons/calendar icon.png';
   static const String jobsIcon = 'assets/icons/jobs_icon.png';
   static const String settingsIcon = 'assets/icons/settings_icon.png';
+  static const String dumbbellIcon = 'assets/icons/dumbbell.png';
+  static const String runningIcon = 'assets/icons/running.png';
 }
--- lib/view/core/style/color_manager.dart
+++ lib/view/core/style/color_manager.dart
@@ -2,5 +2,7 @@ import 'dart:ui';
 
 class ColorManager {
   static const Color lightGrey = Color(0xFFEBEBEB);
-  static const Color grey = Color(0xFFC2C2C2);
+  static const Color grey = Color(0xFF686868);
+  static const Color lightCyan = Color(0xFF67F5FF);
+  static const Color darkCyan = Color(0xFF158289);
 }
